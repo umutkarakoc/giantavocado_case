a6d3afd67d4107978a1347d4c9c74a957c56dd20
umut <umut@sent.com>
2022-09-12 14:43:04 +0300

    remove and add comments

--- research.cs
+++ research.cs
@@ -23,26 +23,16 @@ namespace giantavocado
 				[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
 				ILogger log)
 		{
-			// log.LogInformation("C# HTTP trigger function processed a request.");
-
-			// 
-
-
-			// name = name ?? data?.name;
-
-			// string responseMessage = string.IsNullOrEmpty(name)
-			//     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-			//     : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
 			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 			dynamic data = JsonConvert.DeserializeObject(requestBody);
 			string userId = data.userId;
 
-
-
 			using (var httpClient = new HttpClient())
 			{
+				// playfab secretkey'i azure settings'den geliyor
 				httpClient.DefaultRequestHeaders.Add("X-SecretKey", Environment.GetEnvironmentVariable("PLAYFAB_SECRET"));
+
 				HttpResponseMessage res = await httpClient.PostAsync(
 					"https://ECEE9.playfabapi.com/Server/GetTitleInternalData", 
 					new StringContent("{\"Keys\": [\"ResearchConfig\"]}", Encoding.UTF8, "application/json")
@@ -57,6 +47,7 @@ namespace giantavocado
 					res.Content.ReadAsStringAsync().Result
 				);
 			
+				// response icerisinde data.Data.ResearchConfig bir string deger ve bizim config ayarlarimizi tutuyor.
 				ResearchConfig config = JsonConvert.DeserializeObject<ResearchConfig>((string)result.data.Data.ResearchConfig);
 
 				var userData = new ResearchUserData();
